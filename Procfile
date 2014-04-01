7964afc8882db1001798b4fb6a5f7d5258b7944e
Ben Firshman <ben@firshman.co.uk>
2014-04-01 11:44:33 +0100

    Set up for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn documentchain.server:app
--- documentchain/server.py
+++ documentchain/server.py
@@ -4,10 +4,14 @@ from .chain import DocumentChain
 from .storage import RedisStorage
 
 app = Flask(__name__)
-storage = RedisStorage('redis://%s:%s' % (
-    os.environ.get('REDIS_1_PORT_6379_TCP_ADDR'),
-    os.environ.get('REDIS_1_PORT_6379_TCP_PORT')
-))
+
+if 'REDISCLOUD_URL' in os.environ:
+    storage = RedisStorage(os.environ['REDISCLOUD_URL'])
+else:
+    storage = RedisStorage('redis://%s:%s' % (
+        os.environ.get('REDIS_1_PORT_6379_TCP_ADDR'),
+        os.environ.get('REDIS_1_PORT_6379_TCP_PORT')
+    ))
 chain = DocumentChain(storage)
 
 @app.route('/entries', methods=['GET', 'POST'])
