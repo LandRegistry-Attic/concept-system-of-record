fedcfa943ba153105972e2eeed32feff7bfa14d7
Ben Firshman <ben@firshman.co.uk>
2014-05-06 16:56:26 +0100

    Add list of altered records to dashboard

--- systemofrecord/static/javascripts/dashboard-verifier.js
+++ systemofrecord/static/javascripts/dashboard-verifier.js
@@ -37,18 +37,37 @@
     },
     setValid: function() {
       this.$el.find('.status-text').text('All records intact')
+      this.$el.find('.altered-records').html('');
       this.$el.find('.big-icon')
         .removeClass('glyphicon-remove')
         .addClass('glyphicon-ok');
     },
     setInvalid: function(entries) {
       if (entries.length == 1) {
-        var statusText = '1 record is incorrect';
+        var statusText = '1 record has been altered';
       }
       else {
         var statusText = entries.length + ' records are incorrect';
       }
       this.$el.find('.status-text').text(statusText);
+
+
+      var $table = $('<table class="table"><thead><tr><th>Version</th><th>Title number</th><th>Address</th></tr></thead><tbody></tbody></table>');
+
+      var rowTemplate = _.template('<tr><td><%- id %></td><td><%- title_number %></td><td><%- address %></td>');
+
+      _.each(entries, function(entry) {
+        $table.find('tbody').append(rowTemplate({
+          id: entry.id.slice(0, 16) + '...',
+          title_number: entry.content.title_number,
+          address: entry.content.address,
+        }));
+      });
+      var entry = entries[0];
+      this.$el.find('.altered-records').append('<p>'+entry.content.title_number+'</p>')
+      this.$el.find('.altered-records').html($table);
+
+
       this.$el.find('.big-icon')
         .removeClass('glyphicon-ok')
         .addClass('glyphicon-remove');
